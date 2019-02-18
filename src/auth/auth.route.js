806553e0d22e37b1a86acac4128b99932d373ec3
Yossi Gartner <yosigartner@gmail.com>
2019-02-18 15:44:48 +0200

    remove the use of 'auth' middleware

--- src/auth/auth.route.js
+++ src/auth/auth.route.js
@@ -4,11 +4,6 @@ import * as authCtrl from "./auth.controller";
 const router = express.Router();
 
 /**
- * Middleware auth verify
- */
-router.use(authCtrl.auth);
-
-/**
  * POST /api/auth/login - Returns token if correct
  */
 router.post("/login", authCtrl.login);
