78de8c6620ff5ff2eb772706e5d7d056be42135d
Yossi Gartner <yosigartner@gmail.com>
2019-02-18 15:46:23 +0200

    add body-parser use & edit serviceAccount path due to path changes add 'debug' script on npm scripts

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import admin from "firebase-admin";
-
+import bodyParser from "body-parser";
 import routes from "./app.route";
-import serviceAccount from "./config/serviceAccountKey.json";
+import serviceAccount from "../config/serviceAccountKey.json";
 
 const app = express();
+app.use(bodyParser.json());
 
 // mount all routes on /api path
 app.use("/api", routes);
