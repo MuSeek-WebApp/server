25318628f12cb56d5e3fe62de9af23f4bcd01706
Yossig <yosigartner@gmail.com>
2019-04-22 23:57:54 +0300

    add update/remove functions to events controller

--- src/event/event.controller.js
+++ src/event/event.controller.js
@@ -20,20 +20,38 @@ exports.findById = (req, res) => {
 exports.insertEvent = async (req, res) => {
   try {
     const newEvent = new EventModel(req.body);
-    await newEvent.save();
-    res.sendStatus(200);
+    res.json(await newEvent.save());
   } catch (error) {
     logger.error(error);
     res.sendStatus(500);
   }
 };
 
-exports.updateEvent = (req, res) => {
-  // TODO: call SRV
-  res.sendStatus(200);
+exports.updateEvent = async (req, res) => {
+  try {
+    await eventModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+      (err, event) => {
+        if (err) throw err;
+        return res.json(event);
+      }
+    );
+  } catch (error) {
+    logger.error(error);
+    res.sendStatus(500);
+  }
 };
 
-exports.removeEvent = (req, res) => {
-  // TODO: call SRV
-  res.sendStatus(200);
+exports.removeEvent = async (req, res) => {
+  try {
+    await eventModel.findByIdAndRemove(req.params.id, (err, event) => {
+      if (err) throw err;
+      return res.sendStatus(200);
+    });
+  } catch (error) {
+    logger.error(error);
+    res.sendStatus(500);
+  }
 };
