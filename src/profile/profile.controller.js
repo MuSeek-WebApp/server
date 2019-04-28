4c7477e956e982acaa9065f750107cd67236e6b7
MatanD <matan@outlook.com>
2019-04-28 19:21:29 +0300

    Profile routes  Logo upload (cloudinary based) process

---
+++ src/profile/profile.controller.js
@@ -0,0 +1,47 @@
+import ProfileService from './profile.srv';
+
+import logger from '../utils/logger';
+
+exports.getUid = async (req, res) => {
+  try {
+    logger.debug('[profile.controller] [getUid]');
+    const { idToken } = req.cookies;
+    const userUid = await ProfileService.getUid(idToken);
+    res.json(userUid);
+  } catch (error) {
+    logger.error(error);
+    res.sendStatus(500);
+  }
+};
+
+exports.getProfile = async (req, res) => {
+  try {
+    logger.debug(`[profile.controller] [getProfile: ${req.body.userId}]`);
+    const userData = await ProfileService.getProfile(req.body.userId);
+    res.json(userData);
+  } catch (error) {
+    logger.error(error);
+    res.sendStatus(500);
+  }
+};
+
+exports.uploadProfileImage = async (req, res) => {
+  try {
+    logger.debug(
+      `[profile.controller] [uploadProfileImage: ${req.files.length}]`
+    );
+    if (req.files[0]) {
+      const { idToken } = req.cookies;
+      let resUrl = {
+        profile_photo: await ProfileService.saveProfileImage(
+          req.files[0],
+          idToken
+        )
+      };
+      res.status(200).json(resUrl);
+    }
+  } catch (error) {
+    logger.error(error);
+    res.sendStatus(500);
+  }
+};
--- src/server.api.route.js
+++ src/server.api.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import eventsRoutes from './events/events.route';
 import bandRoutes from './band/band.route';
+import profileRoutes from './profile/profile.route';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.use('/events', eventsRoutes);
 router.use('/band', bandRoutes);
+router.use('/profile', profileRoutes);
 
 export default router;
