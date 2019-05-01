900034cb6c60b7fad34a6379ecdf05cd58c5da67
Yossi Gartner <yosigartner@gmail.com>
2019-05-01 18:29:28 +0300

    refactor event.srv and event.controller (#24)
    refactor event.srv and event.controller

---
+++ src/event/event.srv.js
@@ -0,0 +1,39 @@
+import { EventModel } from './event.model';
+import logger from '../utils/logger';
+
+class EventService {
+  constructor() {
+    logger.info('EventService initiated.');
+  }
+
+  async all() {
+    return await EventModel.find({}).exec();
+  }
+
+  async getArtistEvents(userId) {
+    return await EventModel.aggregate([
+      { $unwind: '$requests' },
+      { $match: { 'requests.band._id': userId } }
+    ]).exec();
+  }
+
+  async getBusinessEvents(userId) {
+    return await EventModel.find({ 'business._id': userId }).exec();
+  }
+
+  async insert(event, user) {
+    const newEvent = new EventModel(event);
+    newEvent.business = user;
+    return await newEvent.save();
+  }
+
+  async update(event) {
+    return await EventModel.findByIdAndUpdate(event._id, event, { new: true });
+  }
+
+  async remove(id) {
+    await EventModel.findByIdAndRemove(id);
+  }
+}
+
+export default new EventService();
