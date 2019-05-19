0b0deec49f2376dbea55b0939c4060b24a15553e
Yossi Gartner <yosigartner@gmail.com>
2019-05-20 01:33:53 +0300

    add user's reviews CRUD functions (#35)

--- src/server.api.route.js
+++ src/server.api.route.js
@@ -2,11 +2,13 @@ import express from 'express';
 import eventRoutes from './event/event.route';
 import bandRoutes from './band/band.route';
 import profileRoutes from './profile/profile.route';
+import userRoutes from './user/user.route';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.use('/event', eventRoutes);
 router.use('/band', bandRoutes);
 router.use('/profile', profileRoutes);
+router.use('/user', userRoutes);
 
 export default router;
